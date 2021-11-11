eff816763e57ef5fbfe58292fc75b606db9e2fb1
xhoshua <mucaxhoi3@gmail.com>
2021-11-11 17:44:13 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from 'axios'
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+   const getAllUsers =  async () => await axios.get(' http://localhost:3001/users').then((res) => setUsers(res.data));
+   getAllUsers()
+   
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     {users.map((user) => <pre>{user.name}</pre>)}
     </div>
   );
 }
